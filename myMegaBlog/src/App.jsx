ce449a7cdb9c68e2dbe2c4842587425d0cd3b21d
Andronicus Lepcha <andronicus.lepcha@non.keysight.com>
2024-08-13 11:10:20 +0530

    store configured properly || header || footer || provider

--- myMegaBlog/src/App.jsx
+++ myMegaBlog/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  console.log(import.meta.env.VITE_ROBOT_BLOG_URL) 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h2>Robot Blog </h2>
-    </>
+  // useEffect(() => {
+  //   authService.getCurrentUser()
+  //   .then((userData) => {
+  //     if (userData) {
+  //       dispatch(login({userData}))
+  //     } else {
+  //       dispatch(logout())
+  //     }
+  //   })
+  //   .finally(() => setLoading(false))
+  // }, [])
+  // return !loading ? (
+  return(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
   )
 }
 
