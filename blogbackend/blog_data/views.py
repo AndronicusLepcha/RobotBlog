bd3d60c264de7a9a03a1b6605b88884a0bc487b1
Andronicus Lepcha <andronicus.lepcha@non.keysight.com>
2024-08-19 13:31:15 +0530

    get all post api created

--- blogbackend/blog_data/views.py
+++ blogbackend/blog_data/views.py
@@ -5,7 +5,8 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer
+from .serializers import PostDataSerializer, UserSerializer
+from .models import PostData
 
 
 from rest_framework.authtoken.models import Token
@@ -27,7 +28,6 @@ def register_bloguser(request):
     
 @api_view(['POST'])
 def user_login(request):
-    print(request.data)
     username = request.data.get('username')
     password = request.data.get('password')
     
@@ -96,3 +96,16 @@ def createPost(request):
 
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@api_view(['GET'])
+def getAllPost(request):
+    try:
+        if request.method == "GET":
+            print("About the fetch tge postdata")
+            data=PostData.objects.all()
+            print("data is ",data)
+            postData = PostDataSerializer(data,many=True)
+            return Response(postData.data,status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error":"failed to retrieve the all post {e}"},status=status.HTTP_400_BAD_REQUEST)
+        
